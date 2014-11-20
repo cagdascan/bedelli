7a5185860075dec91394758297854107e592de3d
ugunebakan <ugunebakan@yahoo.com>
2014-11-20 17:20:50 +0200

    umut2cagdas

--- client/datepicker.js
+++ client/datepicker.js
@@ -7,5 +7,32 @@ Template.hesaplama.rendered = function() {
     	useCurrent: false,
     	defaultDate: '1/1/1980'
     });
-}
 
+    $(".datetimepicker").on("dp.change",function (e){
+    	var ay= $('.datetimepicker').data("DateTimePicker").viewDate._i.M + 1;
+		var gun= $('.datetimepicker').data("DateTimePicker").viewDate._i.d;
+    	var yil= $('.datetimepicker').data("DateTimePicker").viewDate._i.y;
+
+    	var bedelliYasiTarihi=["15","01","1987"];
+
+    	if ((bedelliYasiTarihi[2]-yil>0)) {
+    		$('#durum').html('Bedelli Tuttu! :)');
+			$('#durum').css('color','green');
+    	}
+    	else if(bedelliYasiTarihi[2]-yil==0 && bedelliYasiTarihi[1]-ay>0){
+			$('#durum').html('Bedelli Tuttu! :)');
+			$('#durum').css('color','green');
+		}
+		else if(bedelliYasiTarihi[2]-yil==0 && bedelliYasiTarihi[1]-ay==0 && bedelliYasiTarihi[0]-gun>=0){
+			
+			$('#durum').html('Bedelli Tuttu! :)');
+			$('#durum').css('color','green');
+		}
+		else {
+			console.log('tutmadi');
+			$('#durum').html('Bedelli Tutmadı :(');
+			$('#durum').css('color','red');
+
+		}
+    });
+}
