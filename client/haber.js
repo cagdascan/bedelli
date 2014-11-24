89aa33576dd216f2f7665ff5c9ab76c43ecb4e97
ugunebakan <ugunebakan@yahoo.com>
2014-11-24 18:00:04 +0200

    haber akisi - haber ekleme

--- client/haber.js
+++ client/haber.js
@@ -1,21 +1,3 @@
-Template.haber.helpers({
-	'haberTarihi': function(){
-		return Haberler.findOne().tarih;
-	},
-
-	'haberBasligim': function(){
-		return Haberler.findOne().haberBasligi;
-	},
-
-	'haberIcerigim': function(){
-		return Haberler.findOne().haberIcerigi;
-	},
-
-	'haberLinkim': function(){
-		return Haberler.findOne().haberLinki;
-	},
-});
-
 Template.haber.listele = function(){
-	return Haberler.find({}, {sort: {sortTime: -1}});
+	return Haberler.find({}, {sort: {numericTime: -1}});
 }
